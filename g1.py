d3a1a1ec0dc3e9655d8262f308a5e72d0ddbb226
vcnovaes <vini2novaes@gmail.com>
2020-04-16 01:17:53 -0300

    Update (16/04/20)
    Adição de placar
    Tela de inicio que espera comando do jogador

--- g1.py
+++ g1.py
@@ -4,13 +4,26 @@ from random import randint
 def colisao(c1,c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
+def direct(k):
+
+    if k  == K_UP:
+        d = 0
+    if k == K_DOWN:
+        d = 1
+    if k == K_RIGHT:
+       d = 3
+    if k == K_LEFT:
+        d = 2
+    return (d)
 
 
+
+score = 0
 up = 0
 down = 1
 left = 2
 right = 3
-
+start = 0
 x = 500
 y = 400
 pygame.init()
@@ -19,6 +32,10 @@ pygame.display.set_caption("Snakepy")
 run = True
 background_img = pygame.image.load('background.bmp').convert()
 game_over = pygame.image.load('gameover.bmp').convert()
+start_img = pygame.image.load('start_scr.bmp').convert()
+font_name = pygame.font.match_font('System',3)
+font  = pygame.font.Font(font_name, 30)
+
 snake = [(260,200),
          (270,200),
          (280,200)]
@@ -27,24 +44,32 @@ skin.fill((15,107,21))
 food = pygame.Surface((10,10))
 p_food = (((randint(30,500))//10)*10,(((randint(30,380)//10)*10)))
 food.fill((255,0,0))
-dir = left
+
 clock = pygame.time.Clock()
+k = 1
+pygame.event.wait()
+screen.blit(start_img, [0, 0])
 while run:
+
     clock.tick(20)
+    score_surface = font.render('SCORE:  %d' %score, True, (233, 233, 233))
+    if k == 1:
+        pygame.event.clear()
+        screen.blit(start_img, [0, 0])
+        pygame.display.flip()
+        pygame.time.wait(500)
+        pygame.event.wait()
+        dir = left
+        k = 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
         if event.type == KEYDOWN:
-            if event.key == K_UP:
-                dir = up
-                #print(dir)
-            if event.key == K_LEFT:
-                dir = left
-            if event.key == K_RIGHT:
-                dir = right
-            if event.key == K_DOWN:
-                dir = down
+            dir = direct(event.key)
+
+
+
 
 
     if dir == up:
@@ -83,10 +108,12 @@ while run:
 
     if colisao(snake[0], p_food):
         p_food = (((randint(30,490))//10)*10,(((randint(30,370)//10)*10)))
+        score = score + 1
         snake.append((0,0))
     #BORDAS
     pygame.draw.rect(screen, (32, 32, 32), ((0, 0), (520, 400)), 25)
     pygame.draw.rect(screen, (153, 153, 0), ((0, 0), (520, 400)), 5)
+    screen.blit(score_surface,[30,30])
 
     for i in range(len(snake)):
          if i > 4:
